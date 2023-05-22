3082105e9c584f1354494595cde4114ab2a15d82
elishevapro <elishevapro@gmail.com>
2023-05-22 09:40:02 +0300

    elisheva add delete message function

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,13 +1,25 @@
-var Message = require('../models/message.js'); 
+var Message = require('../models/message.js');
 
-module.exports={
-    getAll:(req,res)=>{
+module.exports = {
+    getAll: (req, res) => {
         Message.find()
-        .then(messages=>{
-            res.status(200).send(messages)
-        })
-        .catch(error=>{
-            res.status(404).send({error:error.message})
-        })
+            .then(messages => {
+                res.status(200).send(messages)
+            })
+            .catch(error => {
+                res.status(404).send({ error: error.message })
+            })
+    },
+    Delete: (req, res) => {
+        Message.findById(req.params.id)
+            .then(message => {
+                if (!message) {
+                    return res.status(400).send({ message: "Message doesn't found" });
+                }
+                return Message.deleteOne({ _id: req.params.id })
+            })
+            .catch(error => {
+                res.status(404).send({ error: error.message })
+            })
     }
 }
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -3,8 +3,10 @@ const express = require('express')
 const router = express.Router()
 
 const {
-    getAll
+    getAll,
+    Delete
 }=require('../controllers/messageController')
 
 router.get('/',getAll)
+router.delete('/:messageId',Delete)
 module.exports=router
