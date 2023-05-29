712bd67909d6003ebfee87f2416ed579afe51242
elishevapro <elishevapro@gmail.com>
2023-05-29 13:16:49 +0300

    add swagger and swagger_output files using swagger autogen

--- app.js
+++ app.js
@@ -9,16 +9,34 @@ const dotenv = require("dotenv");
 const app = express();
 const port = 3000;
 
-dotenv.config();
-app.use(cors());
-
-// Require and call the Swagger setup function
-require('./swagger')(app)
+const messageRouter = require("./routes/messageRouter");
+const Message = require("./models/message");
 
 const connectionParams = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 };
+
+dotenv.config();
+app.use(cors());
+
+
+const http = require('http');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
+app.use('/doc',swaggerUi.serve,swaggerUi.setup(swaggerFile));
+require('./routes/messageRouter')(app);
+/*
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+*/
+// const open =require('open')
+
+
+// Require and call the Swagger setup function
+// require('./swagger')(app)
 
 mongoose
     .connect(process.env.DB_CONNECTION, connectionParams)
@@ -32,11 +50,10 @@ app.use(bodyParser.json());
 
 app.use(morgan("dev"));
 
-const messageRouter = require("./routes/messageRouter");
-const Message = require("./models/message");
 app.use("/messages", messageRouter);
 
 // Start the server
 app.listen(port, () => {
     console.log(`my app is listening on http://localhost:${port}`);
+    // open('http://localhost:3000/api-docs');
 });
--- swagger.js
+++ swagger.js
@@ -1,26 +1,40 @@
-const swaggerJSDoc = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express')
-
-const options = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'communication multi service',
-            version: '1.0.0'
+const swaggerAutogen = require('swagger-autogen');
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['./routes/messageRouter'];
+const doc = {
+  definitions: {
+    properties: {
+      addMessage: {
+        userCode: {
+          type: "string"
         },
-        servers: [
-            {
-                url: 'http://localhost:3000',
-                description: 'communication multi service server'
-            }
-
-        ]
-    },
-    apis: ['./routes/*.js']
+        subject: {
+          type: "string"
+        },
+        body: {
+          type: "string"
+        }
+      },
+    }
+  },
+  components: {
+    schemas: {
+      message: {
+        properties: {
+          userCode: {
+            type: "string"
+          },
+          subject: {
+            type: "string"
+          },
+          body: {
+            type: "string"
+          }
+        }
+      }
+    }
+  }
 }
-
-const specs = swaggerJSDoc(options)
-
-module.exports = (app) => {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
-}
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+  require('./app');
+});
