a8060a9c55698992ace77cf44ce5155c688ec47c
rivkabenhlomo <133572294+rivkabenhlomo@users.noreply.github.com>
2023-06-01 00:27:59 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,47 +2,62 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
+const logger = require('./logger');
+logger.error('hi')
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
 const app = express();
 const port = 3000;
-const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger_output.json');
-// const messageRouter = require("./routes/messageRouter");
-// const Message = require("./models/message");
 
-app.use(bodyParser.json());
-app.use(morgan("dev"));
 dotenv.config();
+
 app.use(cors());
 
-// Require and call the Swagger setup function
+
 require('./swagger')(app);
 
 const connectionParams = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-require('./routes/messageRouter')(app);
-
-// const open =require('open')
-
+logger.info(process.env.DB_CONNECTION)
 
 mongoose
-    .connect(process.env.DB_CONNECTION, connectionParams)
-    .then(() => {
-        console.log("connect to mongoDB");
-    })
-    .catch((error) => {
-        console.log(error.message);
-    });
+  .connect(process.env.DB_CONNECTION, connectionParams)
+  .then(() => {
+    logger.info("connect to mongoDB");
+  })
+  .catch((error) => {
+    logger.error(error.message);
+  });
+
+app.use(bodyParser.json());
+
+app.use(morgan("dev"));
+
+const messageRouter = require("./routes/messageRouter");
+const Message = require("./models/message");
+app.use("/messages", messageRouter);
 
 
-// app.use("/messages", messageRouter);
-
-app.listen(port, () => {
-    console.log(`my app is listening on http://localhost:${port}`);
-    // open('http://localhost:3000/doc');
+process.on('uncaughtException', (err) => {
+ 
+  logger.fatal(err, 'uncaught exception detected');
+  
+  server.close(() => {
+    process.exit(1); 
+  });
+ 
+  setTimeout(() => {
+    process.abort();
+  }, 1000).unref()
+  process.exit(1);
 });
+
+
+const server = app.listen(port, () => {
+  logger.info(`my app is listening on http://localhost:${port}`);
+});
+
