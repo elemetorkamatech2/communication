93930ce984e94eac749e6f34f390389598459e3c
chanaKastner <chana64065@gmail.com>
2023-06-05 12:39:26 +0300

    a

--- app.js
+++ app.js
@@ -15,29 +15,17 @@ app.use(cors());
 // Require and call the Swagger setup function
 require('./swagger')(app);
 
-const connectionParams = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-};
+const connectionParams = { useNewUrlParser: true, useUnifiedTopology: true };
 
-mongoose
-	.connect(process.env.DB_CONNECTION, connectionParams)
-	.then(() => {
-		console.log('connect to mongoDB');
-	})
-	.catch((error) => {
-		console.log(error.message);
-	});
+mongoose.connect(process.env.DB_CONNECTION, connectionParams).then(() => { console.log('connect to mongoDB'); }).catch((error) => { console.log(error.message); });
 app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
 const messageRouter = require('./routes/messageRouter');
-const Message = require('./models/message');
+// const Message = require('./models/message');
 
 app.use('/messages', messageRouter);
 
 // Start the server
-app.listen(port, () => {
-	console.log(`my app is listening on http://localhost:${port}`);
-});
+app.listen(port, () => { console.log(`my app is listening on http://localhost:${port}`); });
