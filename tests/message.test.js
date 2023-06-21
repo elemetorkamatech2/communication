1002d65da196a6db5db42f40985480351b9c929d
elishevapro <elishevapro@gmail.com>
2023-06-21 21:24:29 +0300

    some fixing

--- tests/message.test.js
+++ tests/message.test.js
@@ -2,9 +2,9 @@
 
 // const express = require('express');
 // const request = require('supertest');
-// const messageRouter = require('../routes/messageRouter');
-// const { getAll, createMessage } = require('../controllers/messageController');
-// const Message = require('../models/message');
+// const messageRouter = require('../api/routes/messageRouter');
+// const { getAll, createMessage } = require('../api/controllers/messageController');
+// const Message = require('../api/models/message');
 
 // const app = express();
 // app.use('/messages', messageRouter);
@@ -27,58 +27,60 @@
 //     expect(res.send).toHaveBeenCalledWith(messages);
 //   });
 
-// test('should return an error message if there are no messages', async () => {
-//   jest.spyOn(Message, 'find').mockRejectedValue(new Error());
+//   test('should return an error message if there are no messages', async () => {
+//     jest.spyOn(Message, 'find').mockRejectedValue(new Error());
 
-//   const req = {};
-//   const res = {
-//     status: jest.fn().mockReturnThis(),
-//     send: jest.fn(),
-//   };
+//     const req = {};
+//     const res = {
+//       status: jest.fn().mockReturnThis(),
+//       send: jest.fn(),
+//     };
 
-//   await getAll(req, res);
+//     await getAll(req, res);
 
-//   expect(res.status).toHaveBeenCalledWith(404);
-//   expect(res.send).toHaveBeenCalledWith('error');
-// });
+//     expect(res.status).toHaveBeenCalledWith(404);
+//     expect(res.send).toHaveBeenCalledWith('error');
+//   });
 // });
 // describe('createMessage function', () => {
-// test('should create a new message', async () => {
-//   const req = {
-//     body: {userCode:"5678",
-//            subject:"ddd",
-//            body:"bbb"},
-//   };
-//   const res = {
-//     status: jest.fn().mockReturnThis(),
-//     send: jest.fn(),
-//   };
+//   test('should create a new message', async () => {
+//     const req = {
+//       body: {
+//         userCode: '5678',
+//         subject: 'ddd',
+//         body: 'bbb',
+//       },
+//     };
+//     const res = {
+//       status: jest.fn().mockReturnThis(),
+//       send: jest.fn(),
+//     };
 
-//   await createMessage(req, res);
+//     await createMessage(req, res);
 
-//   expect(res.status).toHaveBeenCalledWith(200);
-//   expect(res.send).toHaveBeenCalledWith({message: expect.any(Object)});
-// },100000);
+//     expect(res.status).toHaveBeenCalledWith(200);
+//     expect(res.send).toHaveBeenCalledWith({ message: expect.any(Object) });
+//   }, 100000);
 
-// test('should return an error message if there is an error', async () => {
-//   const req = {
-//     body: { text: 'Hello' },
-//   };
-//   const res = {
-//     status: jest.fn().mockReturnThis(),
-//     send: jest.fn(),
-//   };
-//   jest.spyOn(Message.prototype, 'save').mockRejectedValue(new Error('error'));
+//   test('should return an error message if there is an error', async () => {
+//     const req = {
+//       body: { text: 'Hello' },
+//     };
+//     const res = {
+//       status: jest.fn().mockReturnThis(),
+//       send: jest.fn(),
+//     };
+//     jest.spyOn(Message.prototype, 'save').mockRejectedValue(new Error('error'));
 
-// await createMessage(req, res);
+//     await createMessage(req, res);
 
-// expect(res.status).toHaveBeenCalledWith(400);
-// expect(res.send).toHaveBeenCalledWith({ message: 'error' });
-// });
+//     expect(res.status).toHaveBeenCalledWith(400);
+//     expect(res.send).toHaveBeenCalledWith({ message: 'error' });
+//   });
 // });
 
 // test('try get function', async () => {
 //   const res = await request(app).get('/');
 //   expect(res.statusCode).toBe(404);
-// expect(res.send).toBe("error");
+//   expect(res.send).toBe('error');
 // });
